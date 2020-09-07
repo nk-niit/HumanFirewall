c9457cb9efc91a4a0730c5e1eb1323bbc1e3d1bc
navneethnk7 <krishnav199912@gmail.com>
2020-09-07 15:49:16 +0530

    settings.py changes

--- Firewall/settings.py
+++ Firewall/settings.py
@@ -121,11 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
