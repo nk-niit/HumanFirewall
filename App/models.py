4af099a6380ee17b36f506254080900416ee0a4a
navneethnk7 <krishnav199912@gmail.com>
2020-09-30 17:09:51 +0530

    Models.py and UI updated.

--- App/models.py
+++ App/models.py
@@ -15,3 +15,18 @@ from django.db import models
 
 # Navneeth ------------------------------------------------------------
 
+class LandingPage(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    content = models.TextField()
+    #user_id = models.ForeignKey('', on_delete=models.CASCADE)
+
+
+class SendingProfile(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20)
+    _from = models.EmailField(max_length=100, db_column='from')
+    host = models.GenericIPAddressField(protocol='both', unpack_ipv4=False)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=512)
+    #user_id = models.ForeignKey('', on_delete=models.CASCADE)
