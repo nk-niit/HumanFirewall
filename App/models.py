bf298c695481be6dff734fed52398a7332e27750
navneethnk7 <krishnav199912@gmail.com>
2020-09-30 17:20:45 +0530

    New fix

--- App/models.py
+++ App/models.py
@@ -1,26 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-# OVI --------------------------------------------------------------
+class emailtemp(models.Model):
+    tempId = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    tempName = models.CharField(max_length=5000)
+    subject = models.CharField(max_length=5000)
+    text_html = models.TextField()
 
+class campaign(models.Model):
+    campId = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class usergroups(models.Model):
+    usergroupId = models.AutoField(primary_key=True)
+    groupName = models.CharField(max_length=5000)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
-
-
-
-
-
-
-
-# Navneeth ------------------------------------------------------------
+class targets(models.Model):
+    id = models.AutoField(primary_key=True)
+    firstName=models.CharField(max_length=5000)
+    lastName=models.CharField(max_length=5000)
+    email=models.CharField(max_length=5000)
+    position = models.CharField(max_length=5000)
+    userGroupId = models.ForeignKey(usergroups, on_delete=models.CASCADE, default=None)
 
 class LandingPage(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     content = models.TextField()
-    #user_id = models.ForeignKey('', on_delete=models.CASCADE)
-
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class SendingProfile(models.Model):
     id = models.AutoField(primary_key=True)
@@ -29,4 +38,4 @@ class SendingProfile(models.Model):
     host = models.GenericIPAddressField(protocol='both', unpack_ipv4=False)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=512)
-    #user_id = models.ForeignKey('', on_delete=models.CASCADE)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
--- App/views.py
+++ App/views.py
@@ -4,12 +4,16 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib import messages
+import datetime
 
 # Create your views here.
 def dashboard(request):
     if request.session.has_key('username'):
         username = request.session['username']
-        return render(request, "dashboard.html")
+        if (request.method=="GET"):
+            return render(request, "dashboard.html")
+        elif(request.method=="POST"):
+            return HttpResponse("Data Gaya")
     else:
         messages.info(request, 'Kindly Login To Continue')
         return redirect("login")
@@ -21,7 +25,18 @@ def usergroups(request):
     return render(request,"usergroups.html")
 
 def emailtemp(request):
-    return render(request,"emailtemp.html")
+    if request.session.has_key('username'):
+        username = request.session['username']
+        if (request.method=="GET"):
+            return render(request, "emailtemp.html")
+        elif(request.method=="POST"):
+            tempname=request.POST['tempname']
+            subject = request.POST['subject']
+            emailtext = request.POST['emailtext']
+            return HttpResponse(tempname + subject + emailtext + username)
+    else:
+        messages.info(request, 'Kindly Login To Continue')
+        return redirect("login")
 
 def sendingprofile(request):
     return render(request,"sendingprofile.html")
@@ -37,6 +52,7 @@ def login(request):
         username = request.session['username']
         return render(request, "dashboard.html")
     else:
+        #print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
         return render(request,"login.html")
 
 def user_check(request):
