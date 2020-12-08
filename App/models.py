cffb5b0af21719cfb609aaa587bc54bee60cb496
navneethnk7 <krishnav199912@gmail.com>
2020-12-08 14:59:24 +0530

    HF v1.6.40

--- App/models.py
+++ App/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Targets(models.Model):
+class TimeStampMixin(models.Model):
+    class Meta:
+        abstract = True
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+
+class Targets(TimeStampMixin):
     id = models.AutoField(primary_key=True)
     firstName=models.CharField(max_length=50)
     lastName=models.CharField(max_length=50)
@@ -11,20 +18,18 @@ class Targets(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
 
 
-class Campaign(models.Model):
+class Campaign(TimeStampMixin):
     campId = models.AutoField(primary_key=True)
     campaignName = models.CharField(max_length=50)
     emailTemplate = models.IntegerField()
     landingPage = models.IntegerField()
     sendingProfile = models.IntegerField()
     group = models.IntegerField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
     users = models.ManyToManyField(Targets, through='CampaignResults')
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
 
 
-class CampaignResults(models.Model):
+class CampaignResults(TimeStampMixin):
     class Meta:
         unique_together = (('campaign', 'user'),)
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
@@ -32,21 +37,21 @@ class CampaignResults(models.Model):
     userClickStatus = models.BooleanField()
 
 
-class UserGroups(models.Model):
+class UserGroups(TimeStampMixin):
     groupId = models.AutoField(primary_key=True)
     groupName = models.CharField(max_length=50, unique=True)
     totalUsers = models.IntegerField(default=0)
     users = models.ManyToManyField(Targets, through='GroupedUsers')
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
 
-class GroupedUsers(models.Model):
+class GroupedUsers(TimeStampMixin):
     class Meta:
         unique_together = (('user', 'group'),) 
     user = models.ForeignKey(Targets, on_delete=models.CASCADE)
     group = models.ForeignKey(UserGroups, on_delete=models.CASCADE)
 
 
-class EmailTemp(models.Model):
+class EmailTemp(TimeStampMixin):
     tempId = models.AutoField(primary_key=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
     tempName = models.CharField(max_length=50, unique=True)
@@ -54,19 +59,18 @@ class EmailTemp(models.Model):
     text_html = models.TextField()
 
 
-class LandingPage(models.Model):
+class LandingPage(TimeStampMixin):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     filename = models.CharField(max_length=50, unique=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
 
 
-class SendingProfile(models.Model):
+class SendingProfile(TimeStampMixin):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     _from = models.EmailField(max_length=100, db_column='from')
     host = models.CharField(max_length=100)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=512)
-    created_on = models.DateTimeField(auto_now_add=True)
     userId = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_id')
