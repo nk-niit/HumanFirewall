adee803df1cf4897a720bf4dd42e3d5eea67a6b3
navneethnk7 <krishnav199912@gmail.com>
2020-12-08 11:29:42 +0530

    HF v1.6.22

--- Firewall/settings.py
+++ Firewall/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'icsaeliq(@w!j46wuyha7&hnbl*^m6)hxiq_gxjdi_$e!a6)8p'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #hello ouvais
