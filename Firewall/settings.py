7c14389b2c8c91817b4e9e97503cb4eeeca2786d
navneethnk7 <krishnav199912@gmail.com>
2020-12-08 12:28:31 +0530

    HF v1.6.25

--- Firewall/settings.py
+++ Firewall/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+SECRET_KEY = 'icsaeliq(@w!j46wuyha7&hnbl*^m6)hxiq_gxjdi_$e!a6)8p'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #hello ouvais
@@ -72,10 +74,21 @@ WSGI_APPLICATION = 'Firewall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'human_firewall',
+        'USER': 'postgres',
+        'PASSWORD': 'welcome@hf',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
